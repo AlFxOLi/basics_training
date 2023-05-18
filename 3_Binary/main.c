3bb7ef124996b0441bc0e9746f9c23bf837b3e48
AlFxOLi <Alex.FxO.Lisitskiy@gmail.com>
2023-05-18 15:20:18 +0300

    Binary operations

---
+++ 3_Binary/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    two's complement method for representing negative numbers (signed variables):
+        -flip bits
+        -increment
+    example:
+    5 = 000101
+    -5 = 000101 -> 111010 -> 111011
+                   ^            ^
+                flip bits    increment
+
+    5 + (-5) = 000101
+              +111011
+              -------
+            (1)000000 = 0
+
+    4 + (-2) = 000100
+              +111110
+              -------
+            (1)000010 = 2
+    */
+
+    //Bitwise operations
+    signed int four = 4; // 4 = 000100
+    signed int eight = 8; // 8 = 001000
+    signed int answer = 0; // 0 = 000000
+
+    answer = four & eight; 
+    //   000100
+    // & 001000
+    //   000000 = 0
+    printf("4 AND 8 = %d\n", answer);
+
+    answer = four | eight;
+    //   000100
+    // | 001000
+    //   001100 = 12
+    printf("4 OR 8 = %d\n", answer);
+
+    answer = four ^ eight;
+    //   000100
+    // ^ 001000
+    //   001100 = 12
+    printf("4 XOR 8 = %d\n", answer);
+    
+    answer = four << 1;
+    //   000100 
+    //   << 1
+    //   001000 = 8
+    printf("4 << 1 = %d\n", answer);
+
+    answer = eight >> 1;
+    //   001000 
+    //   >> 1
+    //   000100 = 4
+    printf("8 >> 1 = %d\n", answer);
+
+    return 0;
+}
